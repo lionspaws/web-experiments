81f273bd5347ee750b2499714ed278ba331dff0d
Helen Miles <lionspaws@hotmail.com>
2015-04-16 17:27:04 +0100

    placed a series of rules into a json array and made them selectable with a dropdown menu and update buton, allowing to switch between them in the test html page

--- cellular_automata/cellularAutomata.js
+++ cellular_automata/cellularAutomata.js
@@ -15,85 +15,105 @@ for(var a = 0; a <= (x*y); a++)
 // initialise the pattern from halfway across canvas width
 grid[x/2] = 1;
 
-// choose a rule and set up the variable to hold the result
-var rule = 54;
-var outcome = [];
-
 // for each rule: the input are the same, the result is different
-switch(rule)
+var rules = [
+  {"ruleNumber":30, "outcome":[0,0,0,1,1,1,1,0]},
+  {"ruleNumber":54, "outcome":[0,0,1,1,0,1,1,0]},
+  {"ruleNumber":60, "outcome":[0,0,1,1,1,1,0,0]},
+  {"ruleNumber":62, "outcome":[0,0,1,1,1,1,1,0]},
+  {"ruleNumber":90, "outcome":[0,1,0,1,1,0,1,0]},
+  {"ruleNumber":94, "outcome":[0,1,0,1,1,1,1,0]},
+  {"ruleNumber":102, "outcome":[0,1,1,0,0,1,1,0]},
+  {"ruleNumber":110, "outcome":[0,1,1,0,1,1,1,0]},
+  {"ruleNumber":122, "outcome":[0,1,1,1,1,0,1,0]}
+];
+
+for(rule in rules)
 {
-  case 30: // rule 30: 00011110
-    outcome = [0,0,0,1,1,1,1,0];
-    break;
-  case 54: // rule 54: 00110110
-    outcome = [0,0,1,1,0,1,1,0];
-    break;
-  default: // default empty
-    outcome = [0,0,0,0,0,0,0,0];
+  document.getElementById("ruleSelection").innerHTML += '<option value="' + rules[rule].ruleNumber + '">' + rules[rule].ruleNumber + '</option>';
 }
 
-for(var a = 0; a <= x*y; a++)
+function updateCanvas()
 {
-  // the three cells above decide the outcome for the current cell
-  var cell_l = a - (x + 1);
-  var cell_m = a - x;
-  var cell_r = a - (x - 1);
+  // clear the previous data
+  ctx.clearRect (0,0,x,y);
 
-  // 111 > 0
-  if(grid[cell_l] == 1 && grid[cell_m] == 1 && grid[cell_r] == 1)
+  // choose a rule and set up the variable to hold the result
+  var chosenRule = document.getElementById("ruleSelection").value;
+  var outcome = [0,0,0,0,0,0,0,0];
+
+
+  for(rule in rules)
   {
-    grid[a] = outcome[0];
+    if(rules[rule].ruleNumber == chosenRule)
+    {
+      outcome = rules[rule].outcome;
+    }
   }
 
-  // 110 > 0
-  if(grid[cell_l] == 1 && grid[cell_m] == 1 && grid[cell_r] == 0)
+  for(var a = 0; a <= x*y; a++)
   {
-    grid[a] = outcome[1];
+    // the three cells above decide the outcome for the current cell
+    var cell_l = a - (x + 1);
+    var cell_m = a - x;
+    var cell_r = a - (x - 1);
+
+    // 111
+    if(grid[cell_l] == 1 && grid[cell_m] == 1 && grid[cell_r] == 1)
+    {
+      grid[a] = outcome[0];
+    }
+
+    // 110
+    if(grid[cell_l] == 1 && grid[cell_m] == 1 && grid[cell_r] == 0)
+    {
+      grid[a] = outcome[1];
+    }
+
+    // 101
+    if(grid[cell_l] == 1 && grid[cell_m] == 0 && grid[cell_r] == 1)
+    {
+      grid[a] = outcome[2];
+    }
+
+    // 100
+    if(grid[cell_l] == 1 && grid[cell_m] == 0 && grid[cell_r] == 0)
+    {
+      grid[a] = outcome[3];
+    }
+
+    // 011
+    if(grid[cell_l] == 0 && grid[cell_m] == 1 && grid[cell_r] == 1)
+    {
+      grid[a] = outcome[4];
+    }
+
+    // 101
+    if(grid[cell_l] == 0 && grid[cell_m] == 1 && grid[cell_r] == 0)
+    {
+      grid[a] = outcome[5];
+    }
+
+    // 001
+    if(grid[cell_l] == 0 && grid[cell_m] == 0 && grid[cell_r] == 1)
+    {
+      grid[a] = outcome[6];
+    }
+
+    // 110
+    if(grid[cell_l] == 0 && grid[cell_m] == 0 && grid[cell_r] == 0)
+    {
+      grid[a] = outcome[7];
+    }
   }
 
-  // 101 > 0
-  if(grid[cell_l] == 1 && grid[cell_m] == 0 && grid[cell_r] == 1)
+  // draw the grid
+  for(var a = 0; a <= x*y; a++)
   {
-    grid[a] = outcome[2];
-  }
-
-  // 100 > 1
-  if(grid[cell_l] == 1 && grid[cell_m] == 0 && grid[cell_r] == 0)
-  {
-    grid[a] = outcome[3];
-  }
-
-  // 011 > 1
-  if(grid[cell_l] == 0 && grid[cell_m] == 1 && grid[cell_r] == 1)
-  {
-    grid[a] = outcome[4];
-  }
-
-  // 101 > 1
-  if(grid[cell_l] == 0 && grid[cell_m] == 1 && grid[cell_r] == 0)
-  {
-    grid[a] = outcome[5];
-  }
-
-  // 001 > 1
-  if(grid[cell_l] == 0 && grid[cell_m] == 0 && grid[cell_r] == 1)
-  {
-    grid[a] = outcome[6];
-  }
-
-  // 110 > 0
-  if(grid[cell_l] == 0 && grid[cell_m] == 0 && grid[cell_r] == 0)
-  {
-    grid[a] = outcome[7];
-  }
-}
-
-// draw the grid
-for(var a = 0; a <= x*y; a++)
-{
-  if(grid[a] == 1)
-  {
-    // get the x and y values from the overall grid length
-    ctx.fillRect((a%x),(a/y),1,1);
+    if(grid[a] == 1)
+    {
+      // get the x and y values from the overall grid length
+      ctx.fillRect((a%x),(a/y),1,1);
+    }
   }
 }
