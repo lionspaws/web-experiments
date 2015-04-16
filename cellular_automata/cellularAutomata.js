72b2e93baf288a94b5d1e4fb011dde82e76ab6b3
Helen Miles <lionspaws@hotmail.com>
2015-04-16 13:44:26 +0100

    changed name so one js file can hold all rule, added switch for rules, added rule 54

--- cellular_automata/cellularAutomata.js
+++ cellular_automata/cellularAutomata.js
@@ -12,10 +12,26 @@ for(var a = 0; a <= (x*y); a++)
   grid.push(0);
 }
 
-// initialise the pattern
+// initialise the pattern from halfway across canvas width
 grid[x/2] = 1;
 
-// rule 30
+// choose a rule and set up the variable to hold the result
+var rule = 54;
+var outcome = [];
+
+// for each rule: the input are the same, the result is different
+switch(rule)
+{
+  case 30: // rule 30: 00011110
+    outcome = [0,0,0,1,1,1,1,0];
+    break;
+  case 54: // rule 54: 00110110
+    outcome = [0,0,1,1,0,1,1,0];
+    break;
+  default: // default empty
+    outcome = [0,0,0,0,0,0,0,0];
+}
+
 for(var a = 0; a <= x*y; a++)
 {
   // the three cells above decide the outcome for the current cell
@@ -26,60 +42,51 @@ for(var a = 0; a <= x*y; a++)
   // 111 > 0
   if(grid[cell_l] == 1 && grid[cell_m] == 1 && grid[cell_r] == 1)
   {
-    grid[a] = 0;
+    grid[a] = outcome[0];
   }
 
   // 110 > 0
   if(grid[cell_l] == 1 && grid[cell_m] == 1 && grid[cell_r] == 0)
   {
-    grid[a] = 0;
+    grid[a] = outcome[1];
   }
 
   // 101 > 0
   if(grid[cell_l] == 1 && grid[cell_m] == 0 && grid[cell_r] == 1)
   {
-    grid[a] = 0;
+    grid[a] = outcome[2];
   }
 
   // 100 > 1
   if(grid[cell_l] == 1 && grid[cell_m] == 0 && grid[cell_r] == 0)
   {
-    grid[a] = 1;
+    grid[a] = outcome[3];
   }
 
   // 011 > 1
   if(grid[cell_l] == 0 && grid[cell_m] == 1 && grid[cell_r] == 1)
   {
-    grid[a] = 1;
+    grid[a] = outcome[4];
   }
 
   // 101 > 1
   if(grid[cell_l] == 0 && grid[cell_m] == 1 && grid[cell_r] == 0)
   {
-    grid[a] = 1;
+    grid[a] = outcome[5];
   }
 
   // 001 > 1
   if(grid[cell_l] == 0 && grid[cell_m] == 0 && grid[cell_r] == 1)
   {
-    grid[a] = 1;
+    grid[a] = outcome[6];
   }
 
   // 110 > 0
   if(grid[cell_l] == 0 && grid[cell_m] == 0 && grid[cell_r] == 0)
   {
-    grid[a] = 0;
+    grid[a] = outcome[7];
   }
-
 }
-
-// get a random number from 1-100
-// var roll = Math.floor((Math.random() * 100) + 1);
-//
-// if(roll >= 50)
-// {
-//   grid[a] = 1;
-// }
 
 // draw the grid
 for(var a = 0; a <= x*y; a++)
